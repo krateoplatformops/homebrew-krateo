42d599f056c4835bc2aed186267515b57eeb3e95
krateoplatformops <projectkerberus@kiratech.it>
2021-12-27 15:50:30 +0100

    Brew formula update for krateo version v0.2.11

--- Formula/krateo.rb
+++ Formula/krateo.rb
@@ -5,20 +5,12 @@
 class Krateo < Formula
   desc "Cross OS commandline tool to manage Krateo Platform."
   homepage "https://github.com/krateoplatformops/homebrew-krateo"
-  version "0.2.10"
+  version "0.2.11"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/krateoplatformops/krateo/releases/download/v0.2.10/krateo_0.2.10_darwin_arm64.tar.gz"
-      sha256 "82afbd83ca404180555a1214f32c6801915b82f6c15f5fa9c3cadd945eba3293"
-
-      def install
-        bin.install "krateo"
-      end
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/krateoplatformops/krateo/releases/download/v0.2.10/krateo_0.2.10_darwin_amd64.tar.gz"
-      sha256 "8bc2e78098ecaf3a57ef7d38550791bbf71847112515178c20960a2ae5b53669"
+      url "https://github.com/krateoplatformops/krateo/releases/download/v0.2.11/krateo_0.2.11_darwin_amd64.tar.gz"
+      sha256 "b26ae0b27d6a91e44e503653c8715bd526602567e19ccd4c161a07d7a96bf223"
 
       def install
         bin.install "krateo"
@@ -27,17 +19,9 @@ class Krateo < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/krateoplatformops/krateo/releases/download/v0.2.10/krateo_0.2.10_linux_arm64.tar.gz"
-      sha256 "ca3fe093d68c6840d92824edee4cfc190e16d52aef6729d242a81551d746b12d"
-
-      def install
-        bin.install "krateo"
-      end
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/krateoplatformops/krateo/releases/download/v0.2.10/krateo_0.2.10_linux_amd64.tar.gz"
-      sha256 "37b82ae0c28855686c4fc6a3f4d4554cfa3c4ed271171ae5b5c4346aadaedc2f"
+      url "https://github.com/krateoplatformops/krateo/releases/download/v0.2.11/krateo_0.2.11_linux_amd64.tar.gz"
+      sha256 "323f0806f56dcf6ecf8e3c65cc0238ce053e0eae856bb5519f49153d7c62eb0d"
 
       def install
         bin.install "krateo"
