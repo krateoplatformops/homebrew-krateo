0236d6edef36f4c7e669849ba2fa9ff2ec1a4620
krateoplatformops <projectkerberus@kiratech.it>
2022-02-16 17:59:29 +0100

    Brew formula update for krateo version v0.4.10

--- Formula/krateo.rb
+++ Formula/krateo.rb
@@ -5,12 +5,12 @@
 class Krateo < Formula
   desc "Cross OS commandline tool to manage Krateo Platform."
   homepage "https://github.com/krateoplatformops/homebrew-krateo"
-  version "0.4.9"
+  version "0.4.10"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/krateoplatformops/krateo/releases/download/v0.4.9/krateo_0.4.9_darwin_amd64.tar.gz"
-      sha256 "5abb8c457c05ac28ee3a5cf14066c9607f31929cfebc17c7a1693e9809a095f2"
+      url "https://github.com/krateoplatformops/krateo/releases/download/v0.4.10/krateo_0.4.10_darwin_amd64.tar.gz"
+      sha256 "fc0d91944f2e28daabde621ece7855be180f3d443ceedd6adda6a15dbedc1702"
 
       def install
         bin.install "krateo"
@@ -20,8 +20,8 @@ class Krateo < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/krateoplatformops/krateo/releases/download/v0.4.9/krateo_0.4.9_linux_amd64.tar.gz"
-      sha256 "a1ab491cac28e270a607f985ae891ff4c334c4497dcfa36b15349a54c9b58e90"
+      url "https://github.com/krateoplatformops/krateo/releases/download/v0.4.10/krateo_0.4.10_linux_amd64.tar.gz"
+      sha256 "3df047377502edc69882400fb7362876c2f3349db0dd86dfb89d7ff3e24bcc4f"
 
       def install
         bin.install "krateo"
